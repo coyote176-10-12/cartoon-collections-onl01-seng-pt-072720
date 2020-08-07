fdca865fe245aa2d6cdbac32641450d2a5bf59d6
Javier Aguilar <coyote176@msn.com>
2020-08-07 18:31:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,6 @@ def roll_call_dwarves(array)
     index = 0
   array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
-  # code an argument here
-  # Your code here
 
 def summon_captain_planet(array)
   planeteer_calls = []
@@ -11,8 +9,7 @@ def summon_captain_planet(array)
     planeteer_calls << "#{calls.capitalize}!"
   end
   planeteer_calls
-end   # code an argument here
-  # Your code here
+end 
 
 def long_planeteer_calls# code an argument here
   # Your code here
