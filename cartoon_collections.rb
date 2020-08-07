17317dbae36480974cfd68ae437465c47e5b52d7
Javier Aguilar <coyote176@msn.com>
2020-08-07 18:41:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,12 @@ def long_planeteer_calls(array)
  end
 end
 
+def find_the_cheese2(array, array2)
+  cheese_types = array2
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
 
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
