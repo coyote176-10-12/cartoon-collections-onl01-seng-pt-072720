073dee0e49379e85eaa0fa92dd9783a80740cce0
Javier Aguilar <coyote176@msn.com>
2020-08-07 18:24:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,5 @@
 def roll_call_dwarves(array)
-  i = 0 
-    while i < array.length
-    puts ("#{i + 1} ")
   
-  def roll_call_dwarves(array)
-  i = 0 
-  while i < array.length 
-    puts "#{i + 1}. #{array[i]}"
-    i += 1 
-  end 
 end
   # code an argument here
   # Your code here
