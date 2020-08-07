9619c2728b77d6cb987518bb9506ef943a0986bc
Javier Aguilar <coyote176@msn.com>
2020-08-07 18:42:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,5 +24,5 @@ def find_the_cheese2(array, array2)
 end
 
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  #cheese_types = ["cheddar", "gouda", "camembert"]
 
